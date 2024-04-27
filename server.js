fef82b622032deb0b9ee7e2fb5ae7bfe560e2a24
NetNinjaEngineer <memo5260287@outlook.com>
2024-04-27 12:29:44 +0300

    Implemented validations on request body

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const { body } = require('express-validator')
 const path = require('path')
-
 const app = express()
+const { validationResult } = require('express-validator')
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.json())
-
 
 const port = process.env.PORT || 7000
 
@@ -57,46 +57,86 @@ app.get('/api/students/:id', (req, res) => {
 
 // create new student
 
-app.post('/api/students', (request, response) => {
-    request.body.id = students.length + 1;
-    students.push(request.body);
-    response.json(request.body)
-})
+app.post('/api/students',
+    body('firstName')
+        .notEmpty()
+        .withMessage("The firstName is Required")
+        .isString()
+        .isLength({ max: 8, min: 3 })
+        .withMessage("The Minimum lenght is 3 and maximum is 8"),
+    body('lastName')
+        .notEmpty().withMessage("The LastName is Required")
+        .isString().isLength({ max: 8, min: 3 })
+        .withMessage("The Minimum lenght is 3 and maximum is 8"),
+    body('department')
+        .notEmpty().withMessage("The value of department is Required")
+        .isLength({ max: 2, min: 2 })
+    , (request, response) => {
+
+        const validationResults = validationResult(request);
+
+        if (validationResults.isEmpty()) {
+            request.body.id = students.length + 1;
+            students.push(request.body);
+            response.json(request.body)
+        } else {
+            var errors = validationResults.array();
+            response.status(400)
+                .json({ errors: errors.map(e => e.msg) });
+        }
+
+    })
 
 
 // delete exist student
-app.delete('/api/students/:id', (request, response) => {
-    let studentId = parseInt(request.params.id);
+app.delete('/api/students/:id',
+    (request, response) => {
+        let studentId = parseInt(request.params.id);
 
-    let studentIndex = students.findIndex(std => std.id === studentId);
+        let studentIndex = students.findIndex(std => std.id === studentId);
 
-    if (studentIndex !== -1) {
-        students.splice(studentIndex, 1);
-        response.status(200).json({ message: `Student with ID ${studentId} deleted successfully` });
-    } else {
-        response.status(404).json({ error: `Student with ID ${studentId} not found` });
-    }
+        if (studentIndex !== -1) {
+            students.splice(studentIndex, 1);
+            response.status(200).json({ message: `Student with ID ${studentId} deleted successfully` });
+        } else {
+            response.status(404).json({ error: `Student with ID ${studentId} not found` });
+        }
 
 
-})
+    })
 
 
 // updated existed student
 
-app.put('/api/students/:id', (request, response) => {
-    let studentId = parseInt(request.params.id);
-    let updatedStudent = request.body;
+app.put('/api/students/:id',
+    body('firstName')
+        .notEmpty()
+        .withMessage("The firstName is Required")
+        .isString()
+        .isLength({ max: 8, min: 3 })
+        .withMessage("The Minimum lenght is 3 and maximum is 8"),
+    body('lastName')
+        .notEmpty().withMessage("The LastName is Required")
+        .isString().isLength({ max: 8, min: 3 })
+        .withMessage("The Minimum lenght is 3 and maximum is 8"),
+    body('department')
+        .notEmpty().withMessage("The value of department is Required")
+        .isLength({ max: 2, min: 2 }),
 
-    const index = students.findIndex(student => student.id === studentId);
+    (request, response) => {
+        let studentId = parseInt(request.params.id);
+        let updatedStudent = request.body;
 
-    if (index !== -1) {
-        students[index] = { ...students[index], ...updatedStudent }
-        response.sendStatus(204);
-    } else {
-        response.status(404).json({ error: `student with id: ${studentId} was not found` })
-    }
+        const index = students.findIndex(student => student.id === studentId);
 
-})
+        if (index !== -1) {
+            students[index] = { ...students[index], ...updatedStudent }
+            response.sendStatus(204);
+        } else {
+            response.status(404).json({ error: `student with id: ${studentId} was not found` })
+        }
+
+    })
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
